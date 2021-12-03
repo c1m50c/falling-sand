967531121d31739313b11d26ebde449a16915aeb
c1m50c <terminalityms@gmail.com>
2021-12-02 21:07:02 -0600

    `App` Canvas ✔

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { MutableRefObject, useRef } from "react";
 import TopBar from "./components/TopBar";
 import "./App.css";
 
 function App() {
+    const canvas: MutableRefObject<null | HTMLCanvasElement> = useRef(null);
+    
     return (
         <div className="App">
             <TopBar />
+            <canvas ref={canvas} />
         </div>
     );
 }
