5961d13e5942235c342e4864f6aa81d618c217e2
c1m50c <terminalityms@gmail.com>
2021-12-03 23:36:31 -0600

    `vector::Vector2U`, `Vector2U` Arithmetic Operations 🧮

--- src/cell/mod.rs
+++ src/cell/mod.rs
@@ -1,23 +1,5 @@
-use wasm_bindgen::prelude::*;
 pub(crate) mod cell_chunk;
+pub(crate) mod vector;
 pub(crate) mod cell;
 
-
-#[wasm_bindgen]
-#[derive(Debug, Clone, Copy)]
-pub struct Vector2U {
-    pub x: u32,
-    pub y: u32,
-}
-
-
-#[wasm_bindgen]
-impl Vector2U {
-    #[inline]
-    pub fn new(x: u32, y: u32) -> Self {
-        return Self {
-            x,
-            y,
-        }
-    }
-}
+pub type Vector2U = vector::Vector2U;
